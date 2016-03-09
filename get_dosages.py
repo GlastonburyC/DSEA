a9648d2701a8ec1f6e2cefd1c95c77e62fcfbb30
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2016-03-09 19:30:40 +0000

    Update get_dosages.py

--- get_dosages.py
+++ get_dosages.py
@@ -1,18 +1,19 @@
+######### obtain DOSAGEs ###########
+######################################################################################################################
+
 import sys
 import glob
-
-# sys.argv[1] = directory to dosages containing files *.matrix.maf5 split by chromosome.
-# sys.argv[2] = file which contains col 1 = SNP, col2 = gene
-# sys.argv[3] = file out to write SNP dosages
-
+# sys.argv[2] = dosage directory
+# sys.argv[1] = input file of trans associations
 dosage_files=glob.glob(sys.argv[1]+'*.matrix.maf5')
 
 eqtls={}
 with open(sys.argv[2],'r') as f:
+   header=next(f)
    for line in (line.strip().split() for line in f):
       eqtls[line[1]]=line[0]
 
-snps=set(eqtls.values())
+snps=eqtls.values()
 
 with open(sys.argv[3],'w') as output:
    for x in dosage_files:
@@ -20,3 +21,6 @@ with open(sys.argv[3],'w') as output:
          for line in (line.strip().split() for line in results):
             if line[0] in snps:
                output.write("\t".join(str(x) for x in line)+"\n")
+
+
+######################################################################################################################
